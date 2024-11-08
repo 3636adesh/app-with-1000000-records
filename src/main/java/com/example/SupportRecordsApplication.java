d473d22337be9d3ffbc243add9861dd2cf9002fc
adesh.malunjkar <adesh.malunjkar@upgrad.com>
2024-11-08 18:23:07 +0530

    Added mvn supports

---
+++ src/main/java/com/example/SupportRecordsApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupportRecordsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupportRecordsApplication.class, args);
+	}
+
+}
